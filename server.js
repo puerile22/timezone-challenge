2bda058fbd81192f4d4da47db554e5446d25a926
Peng Gao <puerile22@gmail.com>
2015-02-13 16:10:15 -0800

    added gitignore file

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var restify = require('restify');
 var server = restify.createServer({name: 'answerbook-timezone'});
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
-})
+});
